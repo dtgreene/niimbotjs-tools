8297a3d837f86f642de15f3b73bd8276bd740088
Dylan <dylantgreene92@gmail.com>
2024-06-17 22:16:58 -0500

    Update build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import react from '@vitejs/plugin-react';
